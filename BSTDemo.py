44c3026d1918715d96972153ec5dba4124e64f69
ehixson2017 <ehixson2017@gmail.com>
2022-06-10 12:22:14 -0500

    Commit this

--- BSTDemo.py
+++ BSTDemo.py
@@ -11,17 +11,14 @@ class BinaryTree():
         self.root = root
 
     #traverse to leaf, add node to end
-    def append(self, node, parent=None):
-        if parent == None:
-            parent = self.root
-
-            #if node < parent, go left
+    def append(self, node, parent):
+        #if node < parent, go left
         if node.value < parent.value:
             if parent.left:
                 self.append(node, parent.left)
             else:
                 parent.left = node
-            #if node > parent, go right
+        #if node > parent, go right
         elif node.value > parent.value:
             if parent.right:
                 self.append(node, parent.right)
@@ -29,31 +26,62 @@ class BinaryTree():
                 parent.right = node
         
     def printTree(self, node=None):
-        if node == None:
-            node = self.root
-        print(node.printNode())
-        if node.left:
+        if node:
             self.printTree(node.left)
-        if node.right:
+            print(node.value)
             self.printTree(node.right)
 
-    def traverseTree(self, node=None):
-        if node == None:
-            node = self.root
-        if node.left:
-            self.traverseTree(node.left)
-        if node.right:
-            self.traverseTree(node.right)
-        return node
+    def sortTree(self, list=[]):
+        pass
+    
     #organize tree with root=medianNode. left nodes < root & right nodes. right nodes > root & left nodes
-    def optimizeTree(self):
-        nodes, leftnodes, rightnodes = []
+    def optimizeTree(self, node=None):
+        nodes = []
+        leftnodes, rightnodes = []
 
-        #put all nodes into a list
+        #nested function to turn BST into a list
+        def makeTreeList(node, nodelist=[]):
+            flag = False
+            while node is not None:
+                nodelist.append(node)
+                if node.left is not None:
+                    makeTreeList(node.left, nodelist)
+                    if node.right is not None:
+                        makeTreeList(node.right, nodelist)
+            if node.left is None and node.right is None and flag == False:
+                flag = True
+                node = self.root.right
+                makeTreeList(node, nodelist)
+            return nodelist
+
+        nodes = makeTreeList(node, nodes)
+
         #sort list and set root to midpoint
+        nodes = self.sortTree(nodes)
+        listlength = (len(nodes)-1)        
+        midpoint = listlength/2
+
         #put nodes smaller than root into left list, put nodes greater than root into right list
+        for i in range(0, midpoint):
+            if i < midpoint:
+                leftnodes.append(nodes[i])
+        for q in range(midpoint+1, listlength):
+            if q < listlength:
+                rightnodes.append(nodes[q])
+
+        #place sorted elements back into BST
+        self.root = nodes[midpoint]
+        self.root.right, self.root.left = None
+        for m in range(len((leftnodes)-1), 0, -1):
+            self.append(leftnodes[m])
+        for n in range(0, len((rightnodes)-1)):
+            self.append(rightnodes[n])
 
 
+        
+        
+
+   
 
     def remove():
         pass
